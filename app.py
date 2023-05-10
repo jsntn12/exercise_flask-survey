5934a1af1658ae2140fefb7c7719cca3dfe4d990
jsntn12 <jsantana@nperts-MacBook-Pro.local>
2023-05-10 10:49:56 -0400

    feat: Redirect user to start the survey with question 0

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -8,8 +8,14 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 
 debug = DebugToolbarExtension(app)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 @app.route("/")
 def start_survey():
 
     return render_template("surveyStart.html", survey=survey)
+
+@app.route("/begin", methods=["POST"])
+def redirect_to_questions():
+    return redirect("/question/0")
+
