e58f7d1d875f304cb0b0e08d99dc45871b961300
jsntn12 <jsantana@nperts-MacBook-Pro.local>
2023-05-11 14:29:13 -0500

    style: Removed unnecessary comments and extra space from app

--- app.py
+++ app.py
@@ -2,18 +2,14 @@ from flask import Flask, request, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
-
-responses = []
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 
 debug = DebugToolbarExtension(app)
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
+responses = []
 current_question= {"active" : None}
-
-CURRENT_INDEX = current_question['active']
 
 @app.route("/")
 def start_survey():
@@ -39,10 +35,7 @@ def show_questions(questionID):
         return redirect(f'/question/{len(responses)}')
 
     current_question["active"] = questionID
-    
-    print(f"Survery length is {len(survey.questions) }This is the question ID {questionID}, Status of obj {current_question['active'] }")
     question = survey.questions[questionID]
-
     return render_template(f"question.html", question=question, question_num = questionID)
 
 @app.route("/answer", methods=["POST"])
@@ -52,13 +45,6 @@ def answer():
         return redirect("completed.html")
     return redirect(f"/question/{len(responses)}")
 
-
-
-
-
-
-# app.route("/completed")
 @app.route("/completed")
 def load_completed():
-# load thank you for completing the page 
     return render_template("completed.html")
