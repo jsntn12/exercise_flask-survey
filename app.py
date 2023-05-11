5d12c00451eb763c6667804f425398d37e70019e
jsntn12 <jsantana@nperts-MacBook-Pro.local>
2023-05-11 11:31:34 -0400

    fix: Fixed bug that allowed for users to submit form without any selection

--- app.py
+++ app.py
@@ -10,23 +10,20 @@ app.config["SECRET_KEY"] = "secret"
 debug = DebugToolbarExtension(app)
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
-current_question = None
+current_question= {"active" : None}
 
 @app.route("/")
 def start_survey():
-    current_question = 0
+    current_question['active']= 0
+
     return render_template("/surveyStart.html", survey=survey)
 
 @app.route("/begin", methods=["POST"])
 def redirect_to_questions():
-    print("___-- entered begin----__")
-    # establish count to keep track of the question number asked 
     return redirect("/question/0")
 
 @app.route("/question/<int:questionID>")
 def show_questions(questionID):
-    print("---entered the question route-------")
-    #if the len of response == questions return completed page 
 
     if len(responses) == len(survey.questions):
         return redirect("/completed")
@@ -35,21 +32,17 @@ def show_questions(questionID):
     # elif len(response) != current_question:
     #     return redirect(f"/question/{current_question}")
 
-    print(f"This is the question ID {questionID}")
-    
+    print(f"This is the question ID {questionID}, Status of obj {current_question['active'] }")
+    current_question["active"] = questionID
     question = survey.questions[questionID]
 
-    print("*** right ** before ** the return **")
     return render_template(f"question.html", question=question, question_num = questionID)
 
 @app.route("/answer", methods=["POST"])
 def answer():
-    print("====== in the answer section ======")
     choice = request.form['answer']
     responses.append(request.form["answer"])
 
-
-    print(f"===== This is len of responses {len(responses)}")
     return redirect(f"/question/{len(responses)}")
     # update the current question to + 1
     # get the input 
