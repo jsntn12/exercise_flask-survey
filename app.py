4de8e63d33abbdd4bcce4b966afdf0db61069e8c
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-05-10 21:02:22 -0400

    feat: App will display a thank you message once the survey is completed

--- app.py
+++ app.py
@@ -17,10 +17,24 @@ def start_survey():
 
 @app.route("/begin", methods=["POST"])
 def redirect_to_questions():
+    # establish count to keep track of the question number asked 
     return redirect("/question/0")
 
 @app.route("/question/<int:questionID>")
 def show_questions(questionID):
+
+    #if the len of response == questions return completed page 
     if len(response) == len(survey.questions):
         return redirect("/completed")
+    
+    # corner case - user trying to access question out of order 
+    # corner case answer question before the start button 
 
+    # render template the page with the question using current count of questions 
+
+
+# app.route("/completed")
+@app.route("/completed")
+def load_completed():
+# load thank you for completing the page 
+    return render_template("completed.html")
