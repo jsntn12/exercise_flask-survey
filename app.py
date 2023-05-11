bf38cd411f417b0cccc3535ab31a462bcb0c9934
jsntn12 <jsantana@nperts-MacBook-Pro.local>
2023-05-11 10:21:37 -0400

    feat: Handles the verification of the question and if the numer being asked is correct

--- app.py
+++ app.py
@@ -10,27 +10,49 @@ app.config["SECRET_KEY"] = "secret"
 debug = DebugToolbarExtension(app)
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
+current_question = None
+
 @app.route("/")
 def start_survey():
-
-    return render_template("surveyStart.html", survey=survey)
+    current_question = 0
+    return render_template("/surveyStart.html", survey=survey)
 
 @app.route("/begin", methods=["POST"])
 def redirect_to_questions():
+    print("___-- entered begin----__")
     # establish count to keep track of the question number asked 
     return redirect("/question/0")
 
 @app.route("/question/<int:questionID>")
 def show_questions(questionID):
-
+    print("---entered the question route-------")
     #if the len of response == questions return completed page 
     if len(response) == len(survey.questions):
         return redirect("/completed")
+    elif len(response) == 0:
+        return redirect("/")
+    elif len(response) != current_question:
+        return redirect(f"/question/{current_question}")
+    
+    question = survey.question[questionID]
+
+    return render_template(f"/question.html", question=question, question_num = questionID)
     
     # corner case - user trying to access question out of order 
     # corner case answer question before the start button 
 
     # render template the page with the question using current count of questions 
+
+
+@app.route("/answer")
+def answer():
+    ...
+    # update the current question to + 1
+    # get the input 
+    # save the ansewr to array 
+
+
+
 
 
 # app.route("/completed")
