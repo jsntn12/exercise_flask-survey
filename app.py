12b4faed4ca7db6abcacf7c2da30216c652c70c4
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-05-10 20:01:15 -0400

    feat: Loads page with other questions

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
-RESPONSE_KEY = "response"
+response = []
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
@@ -18,4 +18,9 @@ def start_survey():
 @app.route("/begin", methods=["POST"])
 def redirect_to_questions():
     return redirect("/question/0")
+
+@app.route("/question/<int:questionID>")
+def show_questions(questionID):
+    if len(response) == len(survey.questions):
+        return redirect("/completed")
 
